a0e349de8c57896b098f9fb14ff7b9f814674293
mariasoroka <maria_soroka@outlook.com>
2019-05-09 16:45:52 +0300

    fixed

--- traffic_model.h
+++ traffic_model.h
@@ -16,6 +16,7 @@ public:
     car(){
         car_id = -1;
         position_on_the_current_road = 0;
+        vector<int> route;
         current_road_number_in_the_route = -1;
         acceleration = 0;
         velocity = 0;
