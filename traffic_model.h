7060aaf3988280f5d4b61457b1a205a696df4d47
mariasoroka <maria_soroka@outlook.com>
2019-05-09 16:39:23 +0300

    fixed

--- traffic_model.h
+++ traffic_model.h
@@ -41,7 +41,7 @@ class road{
     car * first_car;
     car * last_car;
     bool priority;
-    int relative_roads_id_1;
+    int relative_road_id_1;
     int relative_road_id_2;
 public:
     road(){
@@ -50,17 +50,18 @@ public:
         first_car = NULL;
         last_car = NULL;
         priority = 0;
-        relative_roads_id_1 = -1;
-        relative_roads_id_1 = -1
+        relative_road_id_1 = -1;
+        relative_road_id_2 = -1;
     }
     road(int road_id, double length, car * first_car, car * last_car, bool priority, int relative_road_id_1, int relative_road_id_2):
-    road_id(road_id), length(length), first_car(first_car), last_car(last_car), priority(priority), relative_roads_id[1](relative_roads_id_1), relative_roads_id[2](relative_roads_id_2){}
+    road_id(road_id), length(length), first_car(first_car), last_car(last_car), priority(priority), relative_road_id_1(relative_road_id_1), relative_road_id_2(relative_road_id_2){}
     void set_road_id(int road_id);
     void set_length(double length);
     void set_first_car(car * first_car);
     void set_last_car(car * last_car);
     void set_priority(bool priority);
-    void set_relative_roads_id(int relative_road_id_1, int relative_road_id_2);
+    void set_relative_road_id_1(int relative_road_id_1);
+    void set_relative_road_id_2(int relative_road_id_2);
     int get_road_id();
     double get_road_length();
     car * get_first_car();
